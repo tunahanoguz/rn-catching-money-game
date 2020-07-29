17e227b702e81c68c8f57dc140e45412461a3b46
tunahanoguz <tunahanog@gmail.com>
2020-07-29 17:22:24 +0300

    Create game screen

---
+++ src/screens/GameScreen/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function GameScreen() {
+    return (
+        <View>
+            <Text>Time: 10s</Text>
+            <Text>Score: 100</Text>
+        </View>
+    );
+}
+
+export default GameScreen;
