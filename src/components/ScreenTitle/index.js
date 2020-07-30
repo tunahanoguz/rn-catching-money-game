b37be96ceba81810f249512db965213aeaaaa0a8
tunahanoguz <tunahanog@gmail.com>
2020-07-30 14:29:40 +0300

    Create screen title text

---
+++ src/components/ScreenTitle/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import styles from './styles';
+
+function ScreenTitle({ children }) {
+    return (
+        <Text style={styles.title}>{ children }</Text>
+    );
+}
+
+export default ScreenTitle;
