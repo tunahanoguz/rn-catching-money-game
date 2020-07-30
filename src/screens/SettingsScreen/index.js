d3d2c2c759feb1ffc153d68c36d365cb8774b6b2
tunahanoguz <tunahanog@gmail.com>
2020-07-30 14:30:02 +0300

    Create settings screen

---
+++ src/screens/SettingsScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScreenContainer, ScreenTitle } from '../../components';
+
+function SettingsScreen() {
+    return (
+        <ScreenContainer>
+            <ScreenTitle>Settings</ScreenTitle>
+        </ScreenContainer>
+    );
+}
+
+export default SettingsScreen;
