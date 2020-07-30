51dfe99df7e3228bc959dee0eb4b1bdd9ecb031d
tunahanoguz <tunahanog@gmail.com>
2020-07-30 15:50:37 +0300

    Update prop types of SettingsItem component

--- src/components/SettingsItem/index.js
+++ src/components/SettingsItem/index.js
@@ -1,33 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
+import styles from "./styles";
+import {SafeAreaView} from "react-native-safe-area-context";
 
-function SettingsItem({ title, settings }) {
+function SettingsItem({ title, settings, state, setSettingState }) {
     return (
         <View style={styles.item}>
             <Text style={styles.itemTitle}>{ title }</Text>
 
-            <View>
-                <View>
-                    <Text style={styles.settingTitle}>{ settings.title }</Text>
-                    <Text style={styles.settingDescription}>{ settings.description }</Text>
-                </View>
+            <SafeAreaView>
+                <FlatList
+                    data={settings}
+                    renderItem={({ item, index }) => (
+                        <View style={styles.settingItem} key={index}>
+                            <View style={styles.leftSettingItem}>
+                                <Text style={styles.settingTitle}>{ item.title }</Text>
+                                <Text style={styles.settingDescription}>{ item.description }</Text>
+                            </View>
 
-                <View style={styles.checkCircleOuter}>
-                    <View style={styles.checkCircleInner} />
-                </View>
-            </View>
+                            <View style={styles.rightSettingItem}>
+                                <TouchableOpacity style={styles.checkCircleOuter} onPress={() => setSettingState(index)}>
+                                    { state === index && <View style={styles.checkCircleInner} /> }
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    )}
+                />
+            </SafeAreaView>
         </View>
     );
 }
 
 SettingsItem.propTypes = {
     title: PropTypes.string.isRequired,
-    settings: PropTypes.arrayOf.shape({
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        state: PropTypes.number.isRequired,
-    }),
+    settings: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+        }),
+    ).isRequired,
+    state: PropTypes.number.isRequired,
+    setSettingState: PropTypes.func.isRequired,
 };
 
 export default SettingsItem;
