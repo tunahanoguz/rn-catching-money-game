cc1f924fd6565b194b0b1bf7e2202792b02136b0
tunahanoguz <tunahanog@gmail.com>
2020-07-30 14:29:28 +0300

    Create screen container view

---
+++ src/components/ScreenContainer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView } from "react-native-safe-area-context";
+import styles from './styles';
+
+function ScreenContainer({ children }) {
+    return (
+        <SafeAreaView style={styles.container}>{ children }</SafeAreaView>
+    );
+}
+
+export default ScreenContainer;
