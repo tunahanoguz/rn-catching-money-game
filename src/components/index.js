a797582cbfcfaf3007a80e2d9284a355d7399312
tunahanoguz <tunahanog@gmail.com>
2020-07-30 16:51:36 +0300

    Add BlockButton component to components barrel file

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export NewGameButton from './NewGameButton';
 export ShowPlayerRankingButton from './ShowPlayerRankingButton';
 export ScreenContainer from './ScreenContainer';
 export ScreenTitle from './ScreenTitle';
+export BlockButton from './BlockButton';
