e248f3aece300188cf519b38fb9d90feac4ee135
tunahanoguz <tunahanog@gmail.com>
2020-07-30 23:21:41 +0300

    Complete SignUpScreen

--- src/screens/SignUpScreen/index.js
+++ src/screens/SignUpScreen/index.js
@@ -1,10 +1,56 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, View } from 'react-native';
+import { KeyboardAvoidingView, View, Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin, statusCodes } from '@react-native-community/google-signin';
 import { ScreenContainer, ScreenTitle, BlockButton, Input } from '../../components';
 
-function SignUpScreen() {
+GoogleSignin.configure({
+    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+    webClientId: '317271745451-n47m3sflctm4juiqdqmku16egkuo7rad.apps.googleusercontent.com',
+});
+
+function SignUpScreen({ navigation }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    function signUp() {
+        auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(() => {
+                navigation.navigate('HomeScreen');
+            })
+            .catch(error => {
+                if (error.code === 'auth/email-already-in-use') {
+                    Alert.alert("Registration Failed!", "The email you entered is already in use.");
+                }
+
+                if (error.code === 'auth/invalid-email') {
+                    Alert.alert("Registration Failed!", "You have entered an invalid email.");
+                }
+
+                Alert.alert("Registration Failed!", error.message);
+            });
+    }
+
+    async function signInWithGoogle() {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            const googleCredential = auth.GoogleAuthProvider.credential(userInfo.idToken);
+            auth().signInWithCredential(googleCredential);
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                Alert.alert("Registration Failed!", "You have closed the Google login screen. Therefore login failed.");
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                // operation (e.g. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                Alert.alert("Registration Failed!", "Google play services are not available. Therefore login failed.");
+            } else {
+                Alert.alert("Registration Failed!", "Login failed for an unknown reason.");
+
+            }
+        }
+    }
 
     return (
         <ScreenContainer>
@@ -17,21 +63,27 @@ function SignUpScreen() {
                     placeholder="Email"
                     value={email}
                     setValue={setEmail}
+                    keyboardType="email-address"
                 />
 
                 <Input
                     placeholder="Password"
                     value={password}
                     setValue={setPassword}
+                    secureText={true}
                 />
 
                 <View style={{ height: 10, }} />
 
                 <BlockButton
-                    func={() => console.log("Success!")}
+                    func={signUp}
                 >
                     Sign Up
                 </BlockButton>
+
+                <View style={{ height: 20, }} />
+
+                <BlockButton func={signInWithGoogle}>Sign Up With Google</BlockButton>
             </KeyboardAvoidingView>
         </ScreenContainer>
     );
