8f53362e4bd12333ecfdc5c4c5dc5b2ec648aebd
tunahanoguz <tunahanog@gmail.com>
2020-07-31 13:10:12 +0300

    Add prop types to score item component

--- src/components/ScoreItem/index.js
+++ src/components/ScoreItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
+import PropTypes from 'prop-types';
 import styles from "./styles";
 
 function ScoreItem({ totalScore, date }) {
@@ -10,5 +11,10 @@ function ScoreItem({ totalScore, date }) {
         </TouchableOpacity>
     );
 }
+
+ScoreItem.propTypes = {
+    totalScore: PropTypes.number.isRequired,
+    date: PropTypes.string.isRequired,
+};
 
 export default ScoreItem;
