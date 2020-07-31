3a1a5e861ad891291bafb02ee4d274be69406019
tunahanoguz <tunahanog@gmail.com>
2020-07-31 17:20:35 +0300

    Add score navigation to tab navigation

--- src/navigations/TabNavigation/index.js
+++ src/navigations/TabNavigation/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { HomeNavigation } from '../../navigations';
-import Text from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { HomeNavigation, ScoreNavigation } from '../../navigations';
 
 function TabNavigation() {
     const Tab = createBottomTabNavigator();
 
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={ HomeNavigation } />
+            <Tab.Screen
+                name="Home"
+                component={ HomeNavigation }
+                options={{ tabBarIcon: ({ color, size }) => <Icon name="home" color={color} size={size} /> }}
+            />
+
+            <Tab.Screen
+                name="Score"
+                component={ ScoreNavigation }
+                options={{ tabBarIcon: ({ color, size }) => <Icon name="activity" color={color} size={size} /> }}
+            />
         </Tab.Navigator>
     );
 }
