fd83c32f21a98eb6a9a1d85e813e254b2db5ab3d
tunahanoguz <tunahanog@gmail.com>
2020-07-31 13:25:59 +0300

    Create online scores screen

---
+++ src/screens/OnlineScoresScreen/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import { ScoreItem } from '../../components';
+
+function OnlineScoresScreen() {
+    const scores = [
+        { id: 'score-1', totalScore: 100, date: '31 Temmuz 2020' },
+        { id: 'score-2', totalScore: 200, date: '1 Ağustos 2020' },
+        { id: 'score-3', totalScore: 300, date: '2 Ağustos 2020' },
+    ];
+
+    return (
+        <FlatList
+            data={scores}
+            renderItem={({ item }) => <ScoreItem scoreID={item.id} totalScore={item.totalScore} date={item.date} />}
+        />
+    );
+}
+
+export default OnlineScoresScreen;
