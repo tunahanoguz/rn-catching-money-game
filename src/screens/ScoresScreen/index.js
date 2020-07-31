044f823c6aaa9fa0bea1d57435711831d5d36df7
tunahanoguz <tunahanog@gmail.com>
2020-07-31 22:31:53 +0300

    Add custom status bar component

--- src/screens/ScoresScreen/index.js
+++ src/screens/ScoresScreen/index.js
@@ -1,9 +1,10 @@
 import React, {Fragment} from 'react';
 import { View, Dimensions, StatusBar } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import OnlineScoresScreen from '../OnlineScoresScreen';
 import OfflineScoresScreen from '../OfflineScoresScreen';
-import {SafeAreaView} from "react-native-safe-area-context";
+import { CustomStatusBar } from '../../components';
 
 function ScoresScreen() {
     const [index, setIndex] = React.useState(0);
@@ -30,18 +31,17 @@ function ScoresScreen() {
     }
 
     return (
-        <SafeAreaView style={{ flex: 1, }}>
-            <StatusBar barStyle='light-content' backgroundColor='black' />
+        <View style={{ flex: 1, }}>
+            <CustomStatusBar barStyle='light-content' backgroundColor='black' />
 
             <TabView
-                style={{ flex: 1, paddingTop: StatusBar.currentHeight, }}
                 navigationState={{ index, routes }}
                 renderScene={renderScene}
                 onIndexChange={setIndex}
                 initialLayout={initialLayout}
                 renderTabBar={TabBarComponent}
             />
-        </SafeAreaView>
+        </View>
     );
 }
 
