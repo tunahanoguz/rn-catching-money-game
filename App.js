a8c4ef9b309e8702556e6d344d92b41c5102db3a
tunahanoguz <tunahanog@gmail.com>
2020-07-31 16:25:06 +0300

    Add tab navigation to App.js

--- App.js
+++ App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from './src/screens';
+import { TabNavigation } from './src/navigations';
 
 function App() {
     const Stack = createStackNavigator();
 
   return (
       <NavigationContainer>
-          <Stack.Navigator>
-              <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-          </Stack.Navigator>
+          <TabNavigation />
       </NavigationContainer>
   );
 }
