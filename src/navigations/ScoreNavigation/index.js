258dc4ef0d49bd33bbc0fdea8dfb8cf82881ea9e
tunahanoguz <tunahanog@gmail.com>
2020-07-31 17:34:11 +0300

    Add score detail screen to score navigation

--- src/navigations/ScoreNavigation/index.js
+++ src/navigations/ScoreNavigation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { ScoresScreen } from '../../screens';
+import { ScoresScreen, ScoreDetailScreen } from '../../screens';
 
 function ScoreNavigation() {
     const Stack = createStackNavigator();
@@ -8,6 +8,7 @@ function ScoreNavigation() {
     return (
         <Stack.Navigator>
             <Stack.Screen name="ScoresScreen" component={ScoresScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="ScoreDetailScreen" component={ScoreDetailScreen} options={{ headerShown: false }} />
         </Stack.Navigator>
     );
 }
