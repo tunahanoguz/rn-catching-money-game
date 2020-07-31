d526c3a5e6255e441b096084639e56ddece1e364
tunahanoguz <tunahanog@gmail.com>
2020-07-31 13:27:45 +0300

    Create offline scores screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,3 +4,4 @@ export SettingsScreen from './SettingsScreen';
 export SignUpScreen from './SignUpScreen';
 export SignInScreen from './SignInScreen';
 export OnlineScoresScreen from './OnlineScoresScreen';
+export OfflineScoresScreen from './OfflineScoresScreen';
