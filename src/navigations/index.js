3bda7cf7abeff908516daa4e2703cb5b72c7f4fc
tunahanoguz <tunahanog@gmail.com>
2020-07-31 22:30:48 +0300

    Create settings navigation

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,3 +1,4 @@
 export HomeNavigation from './HomeNavigation';
 export ScoreNavigation from './ScoreNavigation';
 export TabNavigation from './TabNavigation';
+export SettingsNavigation from './SettingsNavigation';
