dd72ffedd54f40817854304a4fdedd522a4aed80
tunahanoguz <tunahanog@gmail.com>
2020-07-31 12:59:24 +0300

    Create sign in screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,3 +2,4 @@ export HomeScreen from './HomeScreen';
 export GameScreen from './GameScreen';
 export SettingsScreen from './SettingsScreen';
 export SignUpScreen from './SignUpScreen';
+export SignInScreen from './SignInScreen';
