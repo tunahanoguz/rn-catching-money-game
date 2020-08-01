be3bf9a42be87baf2f15d06fdfc8d737b4d63d66
tunahanoguz <tunahanog@gmail.com>
2020-08-01 14:08:31 +0300

    Replace SignInScreen and SignUpScreen in AuthNavigation

--- src/navigations/AuthNavigation/index.js
+++ src/navigations/AuthNavigation/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { SignUpScreen, SignInScreen } from '../../screens';
+import { SignInScreen, SignUpScreen } from '../../screens';
 
 function HomeNavigation() {
     const Stack = createStackNavigator();
 
     return (
         <Stack.Navigator>
-            <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{ headerShown: false }} />
             <Stack.Screen name="SignInScreen" component={SignInScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{ headerShown: false }} />
         </Stack.Navigator>
     );
 }
