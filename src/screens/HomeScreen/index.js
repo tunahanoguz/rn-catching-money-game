23724c070bb7b03afdc03e5ad903659d7891a24b
tunahanoguz <tunahanog@gmail.com>
2020-08-01 11:50:06 +0300

    Add HomeButton component to HomeScreen

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { NewGameButton, ShowPlayerRankingButton } from "../../components";
+import { View, StatusBar } from 'react-native';
+import { HomeButton } from "../../components";
 import styles from "./styles";
+import {getStatusBarHeight} from "react-native-status-bar-height";
 
 function Home() {
+    const statusBarHeight = getStatusBarHeight();
+
     return (
-        <View style={styles.container}>
-            <NewGameButton />
-            <ShowPlayerRankingButton />
-        </View>
+        <>
+            <StatusBar barStyle='dark-content' />
+
+            <View style={{ flex: 1, marginTop: statusBarHeight, padding: 30, flexDirection: 'column', justifyContent: 'space-between', }}>
+                <View style={styles.buttonContainer}>
+                    <HomeButton text="New Game" func={() => console.log("New game!")} />
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <HomeButton bgColor="indigo" text="Show your all scores" func={() => console.log("Show scores!")} />
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <HomeButton bgColor="mediumseagreen" text="Show ratings" func={() => console.log("Show ratings!")} />
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <HomeButton bgColor="mediumblue" text="Show cups" func={() => console.log("Show cups!")} />
+                </View>
+            </View>
+        </>
     );
 }
 
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,11 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 30,
+    buttonContainer: {
+        flex: 0.25,
+        alignSelf: 'stretch',
+        marginVertical: 15,
     },
 });
 
