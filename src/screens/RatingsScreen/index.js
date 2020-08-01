69e43416fc8f50407367aa00ee96016d639c4147
tunahanoguz <tunahanog@gmail.com>
2020-08-01 11:15:18 +0300

    Add new objects into ratings constant

--- src/screens/RatingsScreen/index.js
+++ src/screens/RatingsScreen/index.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { FlatList } from 'react-native';
-import { ScreenContainer, ScreenTitle, RatingItem } from '../../components';
+import { View, FlatList } from 'react-native';
+import { ScreenContainer, ScreenTitle, RatingItem, BlockButton } from '../../components';
 
 function RatingsScreen() {
     const ratings = [
-        { playerUsername: 'tunahanoguz' , totalScore: 500 , date: '10 Ağustos 2020' }
+        { playerUsername: 'tunahanoguz' , totalScore: 500 , date: '10 Ağustos 2020' },
+        { playerUsername: 'johndoe' , totalScore: 100 , date: '11 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
+        { playerUsername: 'exampleusername' , totalScore: 300 , date: '12 Ağustos 2020' },
     ];
 
     return (
         <ScreenContainer>
             <ScreenTitle>Ratings</ScreenTitle>
 
+            <View style={{ height: 10, }} />
+
             <FlatList
                 data={ratings}
                 renderItem={({ item }) => <RatingItem playerUsername={item.playerUsername} totalScore={item.totalScore} date={item.date} />}
             />
+
+            <View style={{ height: 10, }} />
+
+            <BlockButton func={() => console.log("More")}>See More</BlockButton>
         </ScreenContainer>
     );
 }
