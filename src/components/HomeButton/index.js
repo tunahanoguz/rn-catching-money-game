7188d6e7ae4c194eb13e4c2387628eede1eb3a8b
tunahanoguz <tunahanog@gmail.com>
2020-08-02 13:06:33 +0300

    Add icon and update style of HomeButton component

--- src/components/HomeButton/index.js
+++ src/components/HomeButton/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/Feather';
 import styles from "./styles";
 
-function HomeButton({ bgColor, text, func }) {
+function HomeButton({ bgColor, text, iconName, func }) {
     return (
         <TouchableOpacity style={[styles.button, { backgroundColor: bgColor }]} onPress={func}>
             <Text style={styles.buttonText}>{ text }</Text>
+            <Icon name={iconName} color='white' size={26} />
         </TouchableOpacity>
     );
 }
@@ -14,6 +16,7 @@ function HomeButton({ bgColor, text, func }) {
 HomeButton.propTypes = {
     bgColor: PropTypes.string,
     text: PropTypes.string.isRequired,
+    iconName: PropTypes.string.isRequired,
     func: PropTypes.func.isRequired,
 };
 
--- src/components/HomeButton/styles.js
+++ src/components/HomeButton/styles.js
@@ -3,6 +3,9 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     button: {
         flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
         padding: 30,
         backgroundColor: 'red',
         borderRadius: 20,
