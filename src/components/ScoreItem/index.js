d57e9fc92cb6f656b3fd613325734510de9165c8
tunahanoguz <tunahanog@gmail.com>
2020-08-02 10:22:10 +0300

    Add trimmed date to ScoreItem component

--- src/components/ScoreItem/index.js
+++ src/components/ScoreItem/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { useNavigation } from '@react-navigation/native';
+import moment from 'moment';
 import styles from "./styles";
 
 function ScoreItem({ scoreID, totalScore, date }) {
@@ -11,6 +12,8 @@ function ScoreItem({ scoreID, totalScore, date }) {
         navigation.navigate('ScoreDetailScreen', { id: scoreID });
     }
 
+    const trimmedDate = moment(date).format('LL')
+
     return (
         <TouchableOpacity style={styles.item} onPress={goToScoreDetail}>
             <View style={styles.leftScoreItem}>
@@ -18,7 +21,7 @@ function ScoreItem({ scoreID, totalScore, date }) {
                 <Text style={styles.scoreText}>{totalScore}</Text>
             </View>
 
-            <Text style={styles.dateText}>{date}</Text>
+            <Text style={styles.dateText}>{trimmedDate}</Text>
         </TouchableOpacity>
     );
 }
