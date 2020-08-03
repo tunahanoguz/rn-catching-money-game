7e5c3f7d69ba38b49a7bad09ade01ffb5b287db8
tunahanoguz <tunahanog@gmail.com>
2020-08-03 15:25:39 +0300

    Complete home screen

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -4,8 +4,20 @@ import auth from '@react-native-firebase/auth';
 import { getStatusBarHeight } from "react-native-status-bar-height";
 import { HomeButton, HomeButtonContainer } from "../../components";
 
-function Home() {
+function Home({ navigation }) {
     const statusBarHeight = getStatusBarHeight();
+
+    function goToGameScreen() {
+        navigation.navigate('GameScreen');
+    }
+
+    function goToScoresScreen() {
+        navigation.navigate('Scores', { screen: 'ScoresScreen' });
+    }
+
+    function goToRatingsScreen() {
+        navigation.navigate('ScoresScreen');
+    }
 
     return (
         <>
@@ -13,19 +25,19 @@ function Home() {
 
             <View style={{ flex: 1, marginTop: statusBarHeight, padding: 30, flexDirection: 'column', justifyContent: 'space-between', }}>
                 <HomeButtonContainer>
-                    <HomeButton text="New Game" func={() => auth().signOut()} />
+                    <HomeButton text="New Game" iconName='circle' func={goToGameScreen} />
                 </HomeButtonContainer>
 
                 <HomeButtonContainer>
-                    <HomeButton bgColor="indigo" text="Show your all scores" func={() => console.log("Show scores!")} />
+                    <HomeButton bgColor="indigo" text="Show your all scores" iconName='activity' func={goToScoresScreen} />
                 </HomeButtonContainer>
 
                 <HomeButtonContainer>
-                    <HomeButton bgColor="mediumseagreen" text="Show ratings" func={() => console.log("Show ratings!")} />
+                    <HomeButton bgColor="mediumseagreen" text="Show ratings" iconName='trending-up' func={goToRatingsScreen} />
                 </HomeButtonContainer>
 
                 <HomeButtonContainer>
-                    <HomeButton bgColor="mediumblue" text="Show cups" func={() => console.log("Show cups!")} />
+                    <HomeButton bgColor="mediumblue" text="Sign Out" iconName='log-out' func={() => auth().signOut()} />
                 </HomeButtonContainer>
             </View>
         </>
