d6aa3908665b083ae0ea75bc8ffbde25b8e50ccd
tunahanoguz <tunahanog@gmail.com>
2020-08-03 15:24:47 +0300

    Add realm db to offline scores screen

--- src/screens/OfflineScoresScreen/index.js
+++ src/screens/OfflineScoresScreen/index.js
@@ -1,34 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { FlatList, Text, View}  from 'react-native';
 import auth from '@react-native-firebase/auth';
-import firestore from "@react-native-firebase/firestore";
 import { ScoreItem } from '../../components';
+import { getGames } from '../../db';
 
 function OfflineScoresScreen() {
     const [scores, setScores] = useState([]);
+    const userID = auth().currentUser.uid;
 
     useEffect(() => {
-        getScores();
-    }, []);
-
-    async function getScores() {
-        const userID = auth().currentUser.uid;
-        firestore().collection('Scores')
-            .where('gameType', '==', 1)
-            .where('userID', '==', userID)
-            .orderBy('scores.score', 'desc').get()
-            .then(querySnapshot => {
-                querySnapshot.forEach(doc => {
-                    const data = doc.data();
-                    const score = {
-                        id: doc.id,
-                        ...data
-                    };
-                    setScores(sc => [...sc, score]);
+        getGames(userID)
+            .then(games => {
+                games.map(game => {
+                    console.log(game.id);
+                    setScores(score => [...score, game]);
                 });
-            })
-            .catch(error => console.log(error));
-    }
+            });
+    }, []);
 
     return (
         <View style={{ flex: 1, padding: 30, }}>
@@ -37,6 +25,7 @@ function OfflineScoresScreen() {
                 <FlatList
                     data={scores}
                     renderItem={({ item }) => <ScoreItem scoreID={item.id} totalScore={item.scores.score} date={item.date} />}
+                    keyExtractor={(item, index) => index.toString()}
                 />
             ) : (
                 <Text>There is no score yet.</Text>
